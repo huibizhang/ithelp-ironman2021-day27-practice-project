c835ee43cfb4012f991a3e3647fb2a14a0d2d35c
搋兔 <naruto6404@hotmail.com>
2021-09-29 01:12:11 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div :class="[
+      'w-screen h-screen',
+      'flex flex-col',
+      'items-center',
+      'py-5'
+    ]"
+  >
+    <AccordionMenuItem
+      v-for="item in list.items"
+      :key="list.groupName.concat('-',item.name)"
+      :itemName="item.name"
+    >
+      <template v-slot:itemText>
+        {{ item.content }}
+      </template>
+    </AccordionMenuItem>
+
+    <br><br>
+    <AccordionMenuItem
+      v-for="link in faq.links"
+      :key="faq.groupName.concat('-',link.name)"
+      :itemName="link.name"
+    >
+      <template v-slot:itemContent>
+        <div
+          :class="[
+            'p-3',
+            'first:border-0 border-t',
+            'hover:bg-gray-100 transition-all'
+          ]"
+          v-for="content in link.contents"
+          :key="content"
+        >
+          {{ content }}
+        </div>
+      </template>
+    </AccordionMenuItem>
+  </div>
+</template>
+
+<script>
+import AccordionMenuItem from './components/AccordionMenuItem.vue'
+
+export default {
+  data() {
+    return {
+      list: {
+        groupName: "Abouts",
+        items: [
+          { name: "關於兔兔教", content: "不是邪教，但不太正常 (？)。 不過可以為你在此獻上教義 ... (住嘴！)"},
+          { name: "關於 Tailwind CSS", content: "超讚了啦，不用真是太可惜了！"},
+          { name: "關於手風琴", content: "流浪到淡水時有機會可以看到。"},
+          { name: "關於兔兔", content: "你想知道的太多了，去擲筊問神吧！！！"}
+        ]
+      },
+      faq: {
+        groupName: "FAQ",
+        links: [
+          { name: "四大超商", contents: [
+            "7-11","全家","萊爾富","OK",
+          ]},
+          { name: "付款方式", contents: [
+            "現金","ATM","信用卡","LINE PAY","五倍券",
+          ]},
+          { name: "取貨方式", contents: [
+            "宅配","超商取貨"
+          ]},
+        ]
+      }
+    }
+  },
+  components: {
+    AccordionMenuItem,
+  }
+}
+</script>
---
+++ src/components/AccordionMenuItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="w-80">
+    <label
+      :class="[
+        'px-4 py-2',
+        'border border-gray-300',
+        'hover:bg-gray-100',
+        'flex justify-between items-center',
+        'cursor-pointer',
+        'transition-all',
+      ]"
+    >
+      <input type="checkbox" class="sr-only" v-model="checked" />
+      <div 
+        :class="[
+          checked && 'text-green-500',
+          'transition-all'
+        ]"
+      >
+        {{ itemName }}
+      </div>
+      <div
+        :class="[
+          'w-9 h-9',
+          'hover:bg-gray-200',
+          'rounded-full',
+          'flex justify-center items-center',
+          'transition-all',
+        ]"
+      >
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          fill="none" viewBox="0 0 24 24" stroke="currentColor"
+          :class="[
+            'h-6 w-6',
+            checked && '-rotate-180',
+            'transition-all'
+          ]"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+        </svg>
+      </div>
+    </label>
+    <div
+      :class="[
+        checked ? 'max-h-[300px]' : 'max-h-0',
+        'border border-t-0 border-gray-300',
+        'overflow-hidden transition-all'
+      ]"
+    >
+      <slot name="itemContent">
+        <div class="p-4 text-gray-600">
+          <slot name="itemText">
+            內容
+          </slot>
+        </div>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AccordionMenuItem",
+  props: {
+    itemName: {
+      default: "選項",
+    }
+  },
+  data() {
+    return {
+      checked: false,
+    }
+  },
+}
+</script>
